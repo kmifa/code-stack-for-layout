d0a1204d8e1b3c7237d5f9abe87bcd4038b50fe1
shinji <rill0131@gmail.com>
2017-09-30 04:18:15 +0900

    finish layout

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// var editor = ace.edit("ace-api");
+// editor.$blockScrolling = Infinity;
+// editor.setOptions({
+//   enableEmmet: true
+// });
+// editor.setTheme("ace/theme/monokai");
+// editor.getSession().setMode("ace/mode/html");
+// // editor.setFontSize(14);
+// // editor.getSession().setUseWrapMode(true);
+// // editor.getSession().setTabSize(2);
