20012dd2929793826ee90cde8c1fb5152558f25c
shinji <rill0131@gmail.com>
2017-10-01 00:11:08 +0900

    add drag drop func

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-// var editor = ace.edit("ace-api");
-// editor.$blockScrolling = Infinity;
-// editor.setOptions({
-//   enableEmmet: true
-// });
-// editor.setTheme("ace/theme/monokai");
-// editor.getSession().setMode("ace/mode/html");
-// // editor.setFontSize(14);
-// // editor.getSession().setUseWrapMode(true);
-// // editor.getSession().setTabSize(2);
+var editor = ace.edit("ace-api");
+editor.$blockScrolling = Infinity;
+
+editor.setValue(document.documentElement.innerHTML, -1);
+var Emmet = require("ace/ext/emmet"); // important to trigger script execution
+editor.setOption("enableEmmet", true);
+
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/html");
+// editor.setFontSize(14);
+// editor.getSession().setUseWrapMode(true);
+// editor.getSession().setTabSize(2);
